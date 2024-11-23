cb7545c75f65bc4bdbd5a35af9a237d2558a11d4
Naser-Alzaghari <naseralzaghari88@gmail.com>
2024-11-23 17:43:48 +0300

    update

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -4,11 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\SoftDeletes;
 class Order extends Model
 {
     use HasFactory;
-
+    use SoftDeletes;
     protected $fillable = ['user_id', 'order_date', 'status', 'total_amount', 'total_amount_after_discount', 'payment_status', 'pickup_date'];
 
     // Belongs-to relationship with User
